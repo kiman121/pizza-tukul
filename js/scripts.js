e0fec5f2b53e44b40a7ea50974eebf1ab25a6dfa
Patrick Kimanthi <kimanthip@gmail.com>
2021-06-24 23:03:15 +0300

    add validation for the place new order action

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,50 @@
-$(document).ready(function(){
-    $("form#pizza-order").submit(function(event){
-        event.preventDefault();
-    });
+$(document).ready(function () {
+
+  $("form#pizza-order").submit(function (event) {
+    event.preventDefault();
+    var formAction = $("input#form-action").val(),
+    validatioFields = [];
+    
+    if(formAction === "place-new-order"){
+        validatioFields = ["pizza-size", "crust-type", "toppings"];
+        var validated = validateUserInput(validatioFields, "form-alerts");
+        if(validated){
+            console.log("Continue coding");
+        }
+    }
+  });
 });
+
+function validateUserInput(formInputFields, alertDivClass) {
+  var validated = true;
+  $(".validate").removeClass("validate");
+
+  formInputFields.forEach((formInputField) => {
+    var field = formInputField,
+      thisField = $("#" + field),
+      value = thisField.val();
+
+    if (value === "") {
+      validated = false;
+      thisField.addClass("validate");
+
+      alertUser("Fill the missing fields!", alertDivClass, "alert-danger");
+    }
+  });
+
+  return validated;
+}
+
+function alertUser(message, alertDivClass, alertClass) {
+  $("." + alertDivClass).html(message);
+  $("." + alertDivClass)
+    .removeClass("hide-alert")
+    .addClass(alertClass);
+
+  setTimeout(() => {
+    $("." + alertDivClass).empty();
+    $("." + alertDivClass)
+      .removeClass(alertClass)
+      .addClass("hide-alert");
+  }, 3000);
+}
